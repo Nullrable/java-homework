5222893efbaa125ce5227e542f64d668a6f7bd7d
卢苏东 <nhsoft.lsd@hotmail.com>
2020-11-20 23:23:08 +0800

    1. JVM 附加题

--- module-work01-class-s/src/main/java/com/lsd/Boot.java
+++ module-work01-class-s/src/main/java/com/lsd/Boot.java
@@ -18,13 +18,17 @@ public class Boot {
 
     public static void main (String args[])throws Exception {
 
+        //30-基于自定义Classloader实现模块化机制：需要设计模块化机制。
         testPlugin();
 
+        //30-基于自定义Classloader实现类的动态加载和卸载：需要设计加载和卸载。
         testDynamicLoadAndUnload();
 
-        testXlassLoad();
-
+        //20-实现xlass打包的xar（类似class文件打包的jar）的加载：xar里是xlass。
         testXarLoad();
+
+        //10-使用自定义Classloader机制，实现xlass的加载：xlass是作业材料。
+        testXlassLoad();
 
     }
 
