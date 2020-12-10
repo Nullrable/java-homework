a201bc86e725c371eb37fcafb9fdf5d228905080
卢苏东 <nhsoft.lsd@hotmail.com>
2020-12-10 21:09:11 +0800

    Week08 作业

--- module-work08-database3-sharding-xa/src/main/java/com/lsd/modulework08database3shardingxa/ModuleWork08Database3ShardingXaApplication.java
+++ module-work08-database3-sharding-xa/src/main/java/com/lsd/modulework08database3shardingxa/ModuleWork08Database3ShardingXaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class ModuleWork08Database3ShardingXaApplication {
 
 	public static void main(String[] args) {
--- module-work08-database3-sharding-xa/src/main/java/com/lsd/modulework08database3shardingxa/config/DatasourceConfig.java
+++ module-work08-database3-sharding-xa/src/main/java/com/lsd/modulework08database3shardingxa/config/DatasourceConfig.java
@@ -1,19 +1,11 @@
 package com.lsd.modulework08database3shardingxa.config;
 
-import java.io.File;
-import java.io.IOException;
-import java.sql.SQLException;
 import javax.sql.DataSource;
-import org.apache.shardingsphere.driver.api.yaml.YamlShardingSphereDataSourceFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 
 /**
@@ -23,28 +15,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
  * @Modified By：
  */
 @Configuration
-@EnableTransactionManagement
 public class DatasourceConfig {
 
-    public DataSource createDataSource(){
-
-        DataSource dataSource = null;
-        try {
-            dataSource = YamlShardingSphereDataSourceFactory.createDataSource(getFile("/application-sharding-jdbc.yaml"));
-        }catch (SQLException | IOException e){
-            System.out.println("application-sharding-jdbc.yaml loading error");
-        }
-        return dataSource;
-    }
-
-
-    private static File getFile(final String configFile) {
-        return new File(DatasourceConfig.class.getResource(configFile).getFile());
-    }
-
     @Bean
-    public PlatformTransactionManager txManager() {
-        return new DataSourceTransactionManager(createDataSource());
+    public PlatformTransactionManager txManager(final DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
 
     @Bean
