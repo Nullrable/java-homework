47b124f50a0f5924f9c6e6c7b4b5be0bf841e844
卢苏东 <nhsoft.lsd@hotmail.com>
2021-04-11 16:44:35 +0800

    LRU cache

---
+++ module-work11-cache-LRU/src/main/java/com/lsd/cache/lru/test/Test.java
@@ -0,0 +1,28 @@
+package com.lsd.cache.lru.test;
+
+/**
+ * @author nhsoft.lsd
+ */
+public class Test {
+
+    static final int MAXIMUM_CAPACITY = 1 << 30;
+
+    public static void main(String args[]) {
+        System.out.println(tableSizeFor(2)); // output:2
+        System.out.println(tableSizeFor(3)); // output:4
+        System.out.println(tableSizeFor(4)); // output:4
+        System.out.println(tableSizeFor(8)); // output:8
+        System.out.println(tableSizeFor(17)); // output:16
+    }
+
+
+    static final int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= MAXIMUM_CAPACITY) ? MAXIMUM_CAPACITY : n + 1;
+    }
+}
