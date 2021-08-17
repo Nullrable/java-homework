831f2d61f2961fe591e9c46bbb70a7581a9b664b
卢苏东 <lusd@cnlemon.net>
2021-08-17 14:32:03 +0800

    netty

--- module-work02-nio-netty/src/main/java/com/lsd/nio/v2/NettyServer.java
+++ module-work02-nio-netty/src/main/java/com/lsd/nio/v2/NettyServer.java
@@ -1,12 +1,94 @@
 package com.lsd.nio.v2;
 
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpHeaders;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpResponseStatus;
+import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.HttpVersion;
+import io.netty.handler.codec.http.LastHttpContent;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author nhsoft.lsd
  */
 public class NettyServer {
 
+    public static void main(String[] args) throws InterruptedException {
+
+        ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        NioEventLoopGroup bossEventLoopGroup = new NioEventLoopGroup(3);
+        NioEventLoopGroup workEventLoopGroup = new NioEventLoopGroup(100);
+        serverBootstrap
+                .group(bossEventLoopGroup, workEventLoopGroup)
+                .channel(NioServerSocketChannel.class)
+                .option(ChannelOption.SO_BACKLOG, 128)          // (7)
+                .childOption(ChannelOption.SO_KEEPALIVE, true)
+                .handler(new LoggingHandler(LogLevel.DEBUG))
+                .childHandler(new ChannelInitializer<SocketChannel>() {
+                    @Override
+                    protected void initChannel(SocketChannel ch) throws Exception {
+                        ChannelPipeline pipeline = ch.pipeline();
+                        pipeline.addLast(new HttpServerCodec());// http 编解码
+                        pipeline.addLast(new HttpObjectAggregator(512*1024)); // http 消息聚合器
+                        pipeline.addLast(new SimpleChannelInboundHandler<FullHttpRequest >() {
+
+                            @Override
+                            public void channelReadComplete(ChannelHandlerContext ctx) {
+                                ctx.flush();
+                            }
+
+                            @Override
+                            protected void channelRead0(ChannelHandlerContext ctx, FullHttpRequest req) {
+
+                                System.out.println("======>channelRead0");
+
+                                String message = "hello,lusudong";
+                                ByteBuf outByteBuf = Unpooled.copiedBuffer(message,  Charset.forName("UTF-8"));
+
+                                FullHttpResponse response = new DefaultFullHttpResponse(HttpVersion.HTTP_1_1, HttpResponseStatus.OK, outByteBuf);
+                                response.headers().set(HttpHeaders.Names.CONTENT_TYPE, "text/html; charset=UTF-8");
+                                response.headers().set(HttpHeaders.Names.CONTENT_LENGTH, message.getBytes(StandardCharsets.UTF_8).length);
+
+                                ctx.write(response);
+
+                                // 写入文件尾部
+                                ChannelFuture future = ctx.writeAndFlush(LastHttpContent.EMPTY_LAST_CONTENT);
+                                future.addListener(ChannelFutureListener.CLOSE);
+                            }
+                        });
+                    }
+                }); // (8)
+
+        // Bind and start to accept incoming connections.
+        ChannelFuture f = serverBootstrap.bind(8088).sync(); // (9)
+        System.out.println("server start up on port : " + 8088);
+
+        // Wait until the server socket is closed.
+        // In this example, this does not happen, but you can do that to gracefully
+        // shut down your server.
+        f.channel().closeFuture().sync();
+
+    }
 }
