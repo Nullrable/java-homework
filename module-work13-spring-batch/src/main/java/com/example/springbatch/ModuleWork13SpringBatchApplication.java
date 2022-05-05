b9fecaaf46b799ef555a4a63c167eeac7adb5870
lusudong <nhsoft.lsd@hotmail.com>
2022-05-05 15:11:19 +0800

    spring batch demo

---
+++ module-work13-spring-batch/src/main/java/com/example/springbatch/ModuleWork13SpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleWork13SpringBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleWork13SpringBatchApplication.class, args);
+    }
+
+}
